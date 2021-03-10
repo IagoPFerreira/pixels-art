d5853f397773974f8be85b16ad9f0346025c2607
Iago de Paula <iago.pferreiravr@gmail.com>
2021-03-10 01:10:55 -0300

    Décimo segundo requisito

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 window.onload = function () {
   palleteColor(collorsPallete);
-  creatBoard(5, 5);
 };
 
 const board = document.querySelector('#pixel-board');
 const collorsPallete = ['black', 'red', 'orange', 'yellow'];
 const pallete = document.querySelector('#color-palette');
 const clearBoard = document.querySelector('#clear-board');
-
 
 function changeClass(elementTarget) {
   const element = document.getElementsByClassName('selected');
@@ -21,9 +19,12 @@ function palleteColor(collors) {
   for (let index = 0; index < collors.length; index += 1) {
     const color = document.createElement('div');
     color.className = 'color';
-    color.style.background = collors[index];
     if (collors[index] === 'black') {
       color.className = 'color selected';
+      color.style.background = 'black';
+    } else {
+      color.style.background = `rgb(${Math.floor(Math.random() * 255)},
+       ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
     }
     pallete.appendChild(color);
     color.addEventListener('click', changeClass);
@@ -38,11 +39,11 @@ function changeColor(elementTarget) {
   elementTarget.target.setAttribute('style', `${color}`);
 }
 
-function creatBoard(row, cell) {
-  for (let index = 0; index < row; index += 1) {
+function creatBoard(size) {
+  for (let index = 0; index < size; index += 1) {
     const tableRow = document.createElement('tr');
     board.appendChild(tableRow);
-    for (let index2 = 0; index2 < cell; index2 += 1) {
+    for (let index2 = 0; index2 < size; index2 += 1) {
       const rowCell = document.createElement('td');
       rowCell.className = 'pixel';
       tableRow.appendChild(rowCell);
@@ -51,11 +52,42 @@ function creatBoard(row, cell) {
   }
 }
 
-function clear(board) {
+function clear() {
   const pixel = document.querySelectorAll('.pixel');
   for (let index = 0; index < pixel.length; index += 1) {
     pixel[index].removeAttribute('style');
   }
 }
 
+// function wipeOutBoard() {
+//   document.getElementById('board-size').reset();
+// }
+
+function verifyBoardSize(boardSizeParameters) {
+  let boardSize = boardSizeParameters;
+
+  if (boardSize <= 5) {
+    boardSize = 5;
+    console.log(boardSize);
+    creatBoard(boardSize);
+  } else if (boardSize > 5) {
+    console.log(boardSize);
+    creatBoard(boardSize);
+  } else if (boardSize > 50) {
+    boardSize = 50;
+    console.log(boardSize);
+    creatBoard(boardSize);
+  } else {
+    alert('Board inválido');
+  }
+}
+
+function boardGenerator() {
+  const boardSize = document.querySelector('#board-size').value;
+  verifyBoardSize(boardSize);
+  // document.getElementById('board-size').reset();
+}
+
+const generateBoard = document.getElementById('generate-board');
+generateBoard.addEventListener('click', boardGenerator);
 clearBoard.addEventListener('click', clear);
